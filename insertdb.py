6c7aeaca9518a0abd6541f3ca1c718d90fa24bc3
akhilgaddam5 <144714253+akhilgaddam5@users.noreply.github.com>
2024-04-17 21:43:13 +0300

    Add files via upload

---
+++ insertdb.py
@@ -0,0 +1,74 @@
+
+# import the database module
+import sqlite3
+
+# connect to a database
+conn = sqlite3.connect('tutorialcustomer.db')
+
+# to connect to a database in memory
+# conn = sqlite3.connect(':memory:')
+
+# Create a custor to work with the database
+cursor = conn.cursor()
+#cursor.execute("DROP TABLE customers")
+
+# Insert data into a table
+cursor.execute("""INSERT INTO customers (first_name, last_name, email) VALUES(
+        "Ankan",
+        "Basu",
+        "basu.ankan@gmail.com"
+        )
+        """
+        )
+
+cursor.execute("""INSERT INTO customers (first_name, last_name, email) VALUES(
+        "Sovon",
+        "Samanta",
+        "samanta@hotmail.com"
+        )
+        """
+        )
+
+cursor.execute("""INSERT INTO customers (first_name, last_name, email) VALUES(
+        "Chandan",
+        "Addya",
+        "chandan@gmail.com"
+        )
+        """
+        )
+
+# Make a list of customers to insert into the database
+customer_list = [
+    ("Black" , "Hat", "Black@gmail.com"),
+    ("White" , "Hat", "white@gmail.com"),
+    ("Grey" , "Hat", "Grey@gmail.com"),
+    ("Green" , "Cap", "Grey@yahoo.com"),
+    ("Yellow" , "Cap", "yello@gmail.com"),
+    ("Orange" , "Cap", "orange@yahoo.com"),
+    ("Blue" , "Cap", "blue@yahoo.com"),
+    ("Purple" , "Cap", "purple@gmail.com"),
+    ("Brown" , "Pal", "brown@outlook.com"),
+    ("Pink" , "Gal", "Pink@outlook.com"),
+    ("Maroon" , "Gal", "Maroon@gmail.com"),
+    ("Light" , "Blue", "Light@blue.com"),
+    ("Dark" , "Blue", "Dark@gblue.com"),
+    ("Dark" , "Brown", "dark@brown.com")
+]
+
+# no need to write a loop!
+cursor.executemany("INSERT INTO customers (first_name, last_name, email) VALUES(?,?,?)", customer_list)
+
+# commit to database
+conn.commit()
+
+# Select data from a table to see if everything is ok
+cursor.execute("SELECT * from customers")
+for row in cursor.fetchall():
+    #print(row)
+    print(row[1] +" " + row[2] + " " + row[3])
+
+conn.commit()
+
+# close connection to the Database
+conn.close()
+
---
+++ selectdb.py
@@ -0,0 +1,92 @@
+
+# import the database module
+import sqlite3
+
+# connect to a database
+conn = sqlite3.connect('tutorialcustomer.db')
+
+# to connect to a database in memory
+# conn = sqlite3.connect(':memory:')
+
+# Create a custor to work with the database
+cursor = conn.cursor()
+cursor.execute("DROP TABLE customers")
+
+print(30*"=")
+print("Select data")
+print(30*"=")
+# Select data from a table
+cursor.execute("SELECT * from customers")
+
+# get one, many or all
+#print(cursor.fetchall())
+#cursor.fetchmany(5)
+#cursor.fetchone()
+
+for row in cursor.fetchall():
+    print(row)
+    print(row[1] +" " + row[2] + " " + row[3])
+
+
+print(30*"=")
+print("Limiting data")
+print(30*"=")
+# Select data from a table
+cursor.execute("SELECT * from customers")
+#cursor.execute("SELECT * from customers LIMIT=2")
+# get one, many or all
+#print(cursor.fetchall())
+#cursor.fetchmany(5)
+#cursor.fetchone()
+
+for row in cursor.fetchall():
+    print(row)
+    print(row[1] +" " + row[2] + " " + row[3])
+
+print(30*"=")
+print("Using a WHERE clause")
+print(30*"=")
+# Using a where clause
+cursor.execute("SELECT * from customers where last_name='Basu'")
+print(cursor.fetchall())
+
+cursor.execute("SELECT * from customers where email like ('%gmail%')")
+for row in cursor.fetchall():
+    #print(row)
+    print(row[1] +" " + row[2] + " " + row[3])
+
+print(30*"=")
+print("Order By")
+print(30*"=")
+# Order by
+cursor.execute("""
+                SELECT * from customers where email like ('%gmail%')
+                ORDER BY first_name desc
+                """)
+for row in cursor.fetchall():
+    #print(row)
+    print(row[1] +" " + row[2] + " " + row[3])
+
+
+print(30*"=")
+print("Conditionals")
+print(30*"=")
+cursor.execute("""
+                SELECT * from customers where email like ('%gmail%')
+                AND id > 5
+                ORDER BY first_name desc
+                """)
+for row in cursor.fetchall():
+    #print(row)
+    print(row[1] +" " + row[2] + " " + row[3])
+
+
+
+
+
+# commit to database
+conn.commit()
+
+# close connection to the Database
+conn.close()
+
